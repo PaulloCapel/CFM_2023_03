0ddfab9a63871398b2e013e1118aaabe402c4f0d
Paulo <paulo.capel@gasparinimercosul.com.br>
2023-09-18 08:04:41 -0300

    initial remote commit

---
+++ CLP/OLD/CLP_UDINESE_REV1/CLP_UDINESE_REV1.ex
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<Configuration MasterDeviceType="50">
+  <TotalData>
+    <Ports>
+      <Port Index="1">
+        <Tables>
+          <Table ProtocolType="Modbus" Mode="0" AutoScan="0">
+            <Blocks/>
+          </Table>
+        </Tables>
+      </Port>
+      <Port Index="2">
+        <Tables>
+          <Table ProtocolType="ModbusTCP" Mode="0">
+            <Blocks/>
+          </Table>
+        </Tables>
+      </Port>
+    </Ports>
+  </TotalData>
+</Configuration>
